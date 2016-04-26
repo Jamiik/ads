58314bacc40257f8ba809c964b71c99a465975ee
giannurman <giannurman10@gmail.com>
2016-04-27 05:20:26 +0700

    Bux fixing. Static header navigation.

--- js/main.js
+++ js/main.js
@@ -149,32 +149,6 @@
 
 	};
 
-
-	// Window Scroll
-	var windowScroll = function() {
-		var lastScrollTop = 0;
-
-		$(window).scroll(function(event){
-
-		   	var header = $('#fh5co-header'),
-				scrlTop = $(this).scrollTop();
-
-			if ( scrlTop > 500 && scrlTop <= 2000 ) {
-				header.addClass('navbar-fixed-top fh5co-animated slideInDown');
-			} else if ( scrlTop <= 500) {
-				if ( header.hasClass('navbar-fixed-top') ) {
-					header.addClass('navbar-fixed-top fh5co-animated slideOutUp');
-					setTimeout(function(){
-						header.removeClass('navbar-fixed-top fh5co-animated slideInDown slideOutUp');
-					}, 100 );
-				}
-			} 
-			
-		});
-	};
-
-
-
 	// Animations
 
 	// About Us
@@ -447,7 +421,6 @@
 		burgerMenu();
 		owlCrouselFeatureSlide();
 		clickMenu();
-		windowScroll();
 		navigationSection();
 
 		aboutWayPoint();
