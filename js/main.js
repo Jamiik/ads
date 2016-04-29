41df6ae70b20568a25eafdf714a0b9a5a7624d52
Baharuddin Aziz <baha.bdg@gmail.com>
2016-04-29 16:23:17 +0700

    Update main.js 1. Edit smarSpeed 2. Edit autoplayTimeout

--- js/main.js
+++ js/main.js
@@ -45,9 +45,9 @@
 		    nav: true,
 		    dots: true,
 		    autoHeight: true,
-		    smartSpeed: 500,
+		    smartSpeed: 1000,
 		    autoplay: true,
-			autoplayTimeout: 5000,
+			autoplayTimeout: 10000,
 			autoplayHoverPause: true,
 		    navText: [	
 		      "<i class='icon-arrow-left2 owl-direction'></i>",
